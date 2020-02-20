e4cf455624d3b01b9282f8a6283c534b79939b34
nyaa-chan <60992185+nyaa-chan@users.noreply.github.com>
2020-02-19 16:37:00 -0800

    update config file and .gitignore, delete old files
    BUPscript.py is depreciated and two mask files have been moved to assets

---
+++ bot/img.py
@@ -0,0 +1,21 @@
+import requests
+from PIL import Image
+
+def Fech(embed):
+	
+	file = requests.get(embed, stream = True)
+	file.raw.decode_content = True
+	
+	return file.raw
+
+def Mask(overlayFile, maskFile, file):
+
+		im = Image.open(file).copy()
+
+		overlay = Image.open(overlayFile).copy()
+		overlay = overlay.resize(im.size)
+		mask = Image.open(maskFile).convert('L').copy().resize(im.size)
+		overlay.putalpha(mask)# apply mask
+		im.paste(overlay, (0,0), overlay)
+
+		return im
